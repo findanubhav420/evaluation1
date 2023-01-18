de3ce11b423d44d6bfaa546a71987b40b591ee04
findanubhav420 <findanubhav420@gmail.com>
2023-01-18 14:53:55 +0530

    feat: add nice variable name

--- bowlingGameCalculator.js
+++ bowlingGameCalculator.js
@@ -1,41 +1,40 @@
-function bowlingScore(input){
-    let n=input.length;
-    let sum=0;
-    let k=0;
-    let count=1;
-    let i=0
-    while(i<n){
-    
-        if(count==10){
-            if(input[i]==10){
-            sum+=input[i]+input[i+1]+input[i+2]
-            i+=3;
+function bowlingScore(input) {
+    let n = input.length;
+    let total_score = 0;
+    let frame_count = 1;
+    let index = 0
+    while (index < n) {
+
+        if (frame_count == 10) {
+            if (input[index] == 10) {
+                total_score += input[index] + input[index + 1] + input[index + 2]
+                index += 3;
+            }
+            else {
+                total_score += input[index] + input[index + 1];
+                index += 2
+            }
         }
-            else{
-            sum+=input[i]+input[i+1];
-            i+=2
+        else if (input[index] == 10) {
+            total_score += input[index] + input[index + 1] + input[index + 2];
+            index++;
+            frame_count++;
         }
+        else if (input[index] + input[index + 1] == 10) {
+            total_score += input[index] + input[index + 1] + input[index + 2];
+            index += 2;
+            frame_count++;
         }
-        else if(input[i]==10){
-            sum+=input[i]+input[i+1]+input[i+2];
-            i++;
-            count++;
+        else {
+            total_score += input[index] + input[index + 1];
+            index += 2
+            frame_count++;
         }
-        else if(input[i]+input[i+1]==10){
-            sum+=input[i]+input[i+1]+input[i+2];
-            i+=2;
-            count++;
-        }
-        else{
-            sum+=input[i]+input[i+1];
-            i+=2
-            count++;
-        }
-       
+
     }
-    return sum;
+    return total_score;
 }
 
-// const score=bowlingScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
+// const score = bowlingScore([10, 1, 5, 1, 2, 10, 5, 2, 10, 10, 8, 1, 10, 3, 2]);
 // console.log(score);
 module.exports=bowlingScore;
