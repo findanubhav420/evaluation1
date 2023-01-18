61d84a18f84e7fb31577a32cac0d5b48e5326b40
findanubhav420 <findanubhav420@gmail.com>
2023-01-18 17:50:36 +0530

    feat: add function to calculate score of the bowling game

--- bowlingGameCalculator.js
+++ bowlingGameCalculator.js
@@ -1,21 +1,25 @@
 function bowlingScore(input) {
-    
+
     let total_throw = input.length;
     let total_score = 0;
     let frame_count = 1;
-    let index = 0
-    if(total_throw<12){
-        throw new Error('not possible');
-    }
-    if(total_throw>21){
-        throw new Error('not possible');
-    }
-    
+    let index = 0;
+    // if(total_throw<12){
+    //     throw new Error('not possile');
+    // }
+    // if(total_throw>21){
+    //     throw new Error('not possible');
+    // }
+
     while (index < total_throw) {
 
         if (frame_count == 10) {
             if (input[index] == 10) {
                 total_score += input[index] + input[index + 1] + input[index + 2]
+                index += 3;
+            }
+            else if (input[index] + input[index + 1] == 10) {
+                total_score += input[index] + input[index + 1] + input[index + 2];
                 index += 3;
             }
             else {
@@ -47,18 +51,20 @@ function bowlingScore(input) {
 
 // 2nd part
 
-// const bestScore=(gameSets)=>{
-//     return gameSets.reduce((maxScore,game)=>{
-//         const gameScore=bowlingScore(game)
-//         maxScore=Math.max(maxScore,gameScore)
+
+// const bestScore = (gameSets) => {
+//     return gameSets.reduce((maxScore, game) => {
+//         let gameScore = bowlingScore(game)
+//         console.log(gameScore);
+//         maxScore = Math.max(maxScore, gameScore)
 
 //         return maxScore;
 //     })
 // }
 
+// const gameSets = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10], [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+// console.log(bestScore(gameSets));
 
 
-
-
-
-module.exports=bowlingScore;
+module.exports = bowlingScore;
